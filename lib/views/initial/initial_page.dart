6cc9754ad31d46dbb33847906748cc342df7b779
Tolga <ttolgabayir@gmail.com>
2023-06-13 15:09:48 +0300

    last commit

--- lib/views/initial/initial_page.dart
+++ lib/views/initial/initial_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:keep_word/controllers/initial/initial_page_controller.dart';
-
+//this page is for local authantication
 class InitialPage extends GetView<InitialPageController> {
   const InitialPage({super.key});
 
